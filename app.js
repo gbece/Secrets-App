a0d1a428a1e8ff8e0cd260bcb2ecdb63f94183ab
Gonzalo <gbecerra94@gmail.com>
2022-12-13 01:55:52 -0300

    New type

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express =require("express");
 const bodyParser = require ("body-parser");
 const ejs =require ("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 =require ("md5");
 
 
 
@@ -25,7 +25,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']  });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,7 +43,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email:req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(!err){
@@ -58,7 +57,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
   const userName= req.body.username;
-  const userPassword=req.body.password;
+  const userPassword=md5(req.body.password);
 
   User.findOne({email:userName},function(err,userFound){
     if(!err){
